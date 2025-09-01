25d475a0560c0a756afdaa18cd5535734dea1b75
BeyzaMutlu200 <azyebultum@gmail.com>
2025-09-01 17:43:56 +0300

    first commit

---
+++ Emlak/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Emlak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Home
+        {
+            public string semt, islem;//islem aktiflik(müşteri var yok ) durumu için kullanılacak
+            public double alan, kira_depozito, satilik_fiyat;
+            public int oda_Sayisi, kat_Sayisi;
+            public DateTime yapim_Tarih;
+
+            public Guid emlak_No;
+            public enum evTür
+            {
+                Daire = 0,
+                Bahçeli = 1,
+                Dubleks = 2,
+                Mustakil = 3
+            }
+
+             ;
+
+            public Home(string semt, string islem, double alan, double kira_depozito, double satilik_fiyat, int oda_Sayisi, int kat_Sayisi, DateTime yapim_Tarih, Guid emlak_No, evTür evtür)
+
+            {
+                this.semt = semt;
+                this.islem = islem;
+                this.alan = alan;
+                this.kira_depozito = kira_depozito;
+                this.satilik_fiyat = satilik_fiyat;
+                this.oda_Sayisi = oda_Sayisi;
+                this.kat_Sayisi = kat_Sayisi;
+                this.yapim_Tarih = yapim_Tarih;
+                this.emlak_No = emlak_No;
+                //this.evtür = evtür; //nenden yanlış anlamadım????
+            }
+            public string Semt
+            {
+                get
+                {
+                    return semt;
+                }
+                set
+                {
+                    semt = value;
+                }
+            }
+            public string Islem
+            {
+                
+                get
+                {
+                    return islem;
+                }
+                set
+                {
+                    islem = value;
+                }
+            }
+            public double Alan
+            {
+                
+                get
+                {
+                    return alan;
+                }
+                set
+                {
+                    try
+                    {
+                        alan = value;
+                       if(value==0 && value < 0)
+                        {
+                            throw new ArgumentException(" Hatalı alan bilgi girişi");
+                        }
+
+                    }
+                    catch (ArgumentException e)
+
+                    {
+                        MessageBox.Show(e.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+                }
+            }
+            public double Kira_Depo
+            {
+                get
+                {
+                    return kira_depozito;
+                }
+                set
+                {
+                    kira_depozito = value;
+                    try
+                    {
+                        if(value<0)
+                        {
+
+                            throw new ArgumentException("Lütfen geçerli bir depozito girişi yapınız");
+                        }
+
+                    }
+                    catch (ArgumentException e)
+
+                    {
+                        MessageBox.Show(e.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+            }
+            public double Satilik_Fiyat
+            {
+                get => satilik_fiyat;
+                set => satilik_fiyat = value;
+
+
+                   
+            }
+            public DateTime Yapim_Tarih
+            {
+                get => yapim_Tarih;
+                set => yapim_Tarih = value;
+            }
+            public int Oda_Sayisi
+            {
+                get => oda_Sayisi;
+                set => oda_Sayisi = value;
+            }
+            public int Kat_Sayisi
+            {
+                get => kat_Sayisi;
+                set => kat_Sayisi = value;
+            }
+            public Guid Emlak_No
+            {
+                get => emlak_No;
+                set => emlak_No = value;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kayitOl f2 = new kayitOl();
+            f2.ShowDialog();
+        }
+    }
+}
---
+++ Emlak/kayitOl.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace Emlak
+{
+    public partial class kayitOl : Form
+    {
+        public List<TextBox> textBoxList;
+        public kayitOl()
+        {
+            InitializeComponent();
+            textBoxList = new List<TextBox>();
+
+            // Tasarım arayüzü üzerinden eklenen TextBox'ları List'e ekle
+            foreach (Control control in Controls)
+            {
+                if (control is TextBox)
+                {
+                    textBoxList.Add((TextBox)control);
+                }
+            }
+        }
+
+        public void kayitOl_Load(object sender, EventArgs e)
+        {
+            kayit IN = new kayit("", "", "", "");
+
+            // Her bir TextBox'ın içeriğini topla
+            IN.Name_sname = textBox1.Text;
+            IN.Email = textBox2.Text;
+            IN.Telno = textBox3.Text;
+
+            IN.Sifre = textBox4.Text;
+        
+            // Toplanan verileri SaveToFile metoduna geçir
+            var result = true;
+            if (result)
+            {
+                try
+                {
+                    // Her bir TextBox için kontrolü gerçekleştir
+                    foreach (var textBox in textBoxList)
+                    {
+                        if (string.IsNullOrWhiteSpace(textBox.Text))
+                        {
+                            throw new ArgumentException("doldurlmayan alan/alanlar mevcut !");
+                        }
+
+                        // Diğer işlemleri buraya ekleyebilirsiniz
+                        Console.WriteLine("Girilen Metin: " + textBox.Text);
+
+                    }
+                    if (string.IsNullOrWhiteSpace(label6.Text) || !label6.Text.ToLower().EndsWith(".pdf"))
+                    {
+                        throw new ArgumentException("Geçerli bir PDF dosyası seçilmelidir!");
+                    }
+                  
+                }
+                catch (ArgumentException ex)
+                {
+                    // TextBox boşsa burası çalışır
+                    MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                //pdfdosyayolu için nasıl ekleme yapabilrim exception hatasını ?
+            }
+            //     MessageBox.Show("Kayıt İşlemi Başarıyla Gerçekleştirildi ", "Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information); 
+            // ekleyince throw mesajından sonra doğru 
+        }
+
+    }
+
+    class kayit
+    {
+        public string name_sname;
+        public string email;
+        public string telno;
+        public string sifre;
+
+        public kayit(string name_sname, string email, string telno, string sifre)
+        {
+            this.name_sname = name_sname;
+            this.email = email;
+            this.telno = telno;
+            this.sifre = sifre;
+        }
+
+        public string Name_sname
+        {
+            get
+            {
+                return name_sname;
+            }
+            set
+            {
+                name_sname = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Telno
+        {
+            get
+            {
+                return telno;
+            }
+            set
+            {
+                telno = value;
+            }
+        }
+
+        public string Sifre
+        {
+            get
+            {
+                return sifre;
+            }
+            set
+            {
+                sifre = value;
+
+            }
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
